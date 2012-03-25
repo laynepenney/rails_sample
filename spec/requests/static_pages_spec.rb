92b8efe781ecddf8f996a16e2ead4bf76de45e03
Layne Penney <layne1penney@gmail.com>
2012-03-25 02:19:45 -0500

    Finishing static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,26 +4,44 @@ describe "StaticPages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do
+    	visit '/home'
+    	page.should have_selector('title', 
+    			:text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "Help page" do
 
-  	it "should have the content 'Help'" do
+  	it "should have the h1 'Help'" do
   		visit '/help'
-  		page.should have_content('Help')
+  		page.should have_selector('h1', :text => "Help")
   	end
+
+    it "should have the right title" do
+    	visit '/help'
+    	page.should have_selector('title', 
+    			:text => "Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe "About Page" do
 
-  	it "should have the content 'About Us'" do
+  	it "should have the h1 'About Us'" do
   		visit '/about'
-  		page.should have_content('About Us')
+  		page.should have_selector('h1', :text => "About Us")
   	end
+
+    it "should have the right title" do
+    	visit '/about'
+    	page.should have_selector('title', 
+    			:text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
   end
 
 end
